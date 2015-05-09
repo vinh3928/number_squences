f45cbe0bd301578caa014a7db9a6b4ed72334ce0
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-05-08 20:44:20 -0600

    finished squence

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 var squence = [1, 2, 4, 8, 16, 32];
 
 var numbSquence = function(array) {
-  var holder = 0, i = 0;
-  while (i < 1) {
-    holder = array[array.length - 1 + i] * 2
+  var holder = 0, i = 0, aLength = 0;
+    aLength = array.length
+  while (i < 10) {
+    holder = array[aLength - 1 + i] * 2
     array.push(holder)
     i ++
+    holder = 0
   }
     return array
 }
 
 console.log(numbSquence(squence));
+
+var aSquence = [ 0, 5, 2, 7, 4, 9]
+var bSquence = [ 1, 2, 4, 8, 16, 32]
+var cSquence = [ 1, 2, 4, 16, 256, 65536]
+var dSquence = [ 1, 4, 7, 10, 13, 16]
+var eSquence = [ 1, 3, 9, 27, 81]
+var fSquence = [ 1, 2, 5, 12, 29]
+
+var randSquence = function(array) {
+    var aL = array.length
+    // for dSquence
+    if (array[2] - array[1] === array[1] - array[0]) {
+      array.push(array[1] - array[0] + array[aL - 1])
+      return array
+    // for bSquence and eSquence
+    } else if (array[aL -1] / array[aL - 2] === array[aL -2] / array[aL -3] && array[aL -2] / array[aL -3] === array[aL -3] / array[aL -4]) {
+      array.push(array[aL-1] * array[aL - 1] / array[aL -2] );
+      return array
+    // for cSquence
+    } else if (Math.sqrt(array[aL -1]) === array[aL -2] && Math.sqrt(array[aL -2]) === array[aL -3]) {
+      array.push(array[aL -1] * array[aL -1])
+      return array
+    // for aSquence
+    } else if (array[aL - 1] - array[aL -2] === array[aL - 3] - array[aL -4]) {
+      array.push(array[aL - 2] - array[aL -3] + array[aL -1])
+      return array
+    // for fSquence
+    } else if (array[aL - 1] - array[aL -2] - array[aL -3] === array[aL -2]) {
+      array.push(array[aL -1] + array[aL -1] + array[aL -2])
+      return array
+    } else {
+      return "I did not account for this squence"
+    }
+}
+
+console.log(randSquence(fSquence));
 
